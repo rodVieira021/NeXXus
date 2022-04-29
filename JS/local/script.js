50402ebf1815ec24d0900612838c6458783e1c24
rodVieira021 <rodvieira021@gmail.com>
2022-04-30 02:12:36 +0300

    modal

--- JS/local/script.js
+++ JS/local/script.js
@@ -1,37 +1,41 @@
-'use strict';
+"use strict";
 
-const btnLight = document.querySelector('.btn-light');
-const btnDark = document.querySelector('.btn-dark');
-const navTrack = document.querySelector('.nav-header');
-const modalMenu = document.querySelector('.menu-right')
-const modal = document.querySelector('.modal')
+const btnLight = document.querySelector(".btn-light");
+const btnDark = document.querySelector(".btn-dark");
+const navTrack = document.querySelector(".nav-header");
+const sliderMenu = document.querySelector(".slider-menu");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".btn-close--m");
+const blur = document.querySelector(".blur");
 
-
-
-btnLight.addEventListener('click', function () {
-  btnLight.style.display = 'none';
-  btnDark.style.display = 'block';
+btnLight.addEventListener("click", function () {
+  btnLight.style.display = "none";
+  btnDark.style.display = "block";
 });
 
-btnDark.addEventListener('click', function () {
-  btnLight.style.display = 'block';
-  btnDark.style.display = 'none';
+btnDark.addEventListener("click", function () {
+  btnLight.style.display = "block";
+  btnDark.style.display = "none";
 });
 
-modalMenu.addEventListener('click', function(){
-  modal.classList.add('modal-show')
-})
+sliderMenu.addEventListener("click", function (e) {
+  e.preventDefault();
+  modal.classList.add("modal-show");
+  blur.classList.add("blur-on");
+});
+
+closeModal.addEventListener("click", function () {
+  modal.classList.remove("modal-show");
+  blur.classList.remove("blur-on");
+});
 
 window.onscroll = () => {
   if (window.scrollY >= 100) {
-    navTrack.style.backgroundColor = '#1f2121';
-    navTrack.style.transition = '0.5s';
+    navTrack.style.backgroundColor = "#1f2121";
+    navTrack.style.transition = "0.5s";
   } else {
-    navTrack.style.backgroundColor = '#9391912b';
-    navTrack.style.backdropFilter = 'blur(0px)';
-    navTrack.style.transition = '0.5s';
+    navTrack.style.backgroundColor = "#9391912b";
+    navTrack.style.backdropFilter = "blur(0px)";
+    navTrack.style.transition = "0.5s";
   }
 };
-
-//menu-right
-
